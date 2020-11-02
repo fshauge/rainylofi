5605ca0b09c64292d5b3c35670a44211d656547b
Markus Hauge <markus.hauge@hotmail.com>
2020-11-02 15:12:14 +0100

    Listen for updates to localStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,14 +1,33 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+
+const deserialize = <T>(value: string) => JSON.parse(value) as T;
+const serialize = <T>(value: T) => JSON.stringify(value);
+
+const createStorage = <T>(key: string) => ({
+  get: () => deserialize<T>(localStorage.getItem(key)!),
+  set: (value: T) => localStorage.setItem(key, serialize(value)),
+  exists: () => key in localStorage,
+});
 
 export const useLocalStorage = <T>(key: string, initialState: T) => {
-  const get = () => JSON.parse(localStorage.getItem(key) as string) as T;
-  const set = (value: T) => localStorage.setItem(key, JSON.stringify(value));
+  const { get, set, exists } = useMemo(() => createStorage<T>(key), [key]);
 
-  if (get() === null) {
+  if (!exists()) {
     set(initialState);
   }
 
   const [state, setState] = useState(get());
+
+  useEffect(() => {
+    const handler = (e: StorageEvent) => {
+      if (e.key === key) {
+        setState(deserialize(e.newValue!));
+      }
+    };
+
+    window.addEventListener("storage", handler);
+    return () => window.removeEventListener("storage", handler);
+  });
 
   return [
     state,
